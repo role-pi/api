e4dd52c26775bb36f333b169d4a5923e2d6f3e40
João Gabriel <joogps@gmail.com>
2023-07-06 17:24:38 -0300

    Adicionar primeiros endpoints

--- src/index.js
+++ src/index.js
@@ -10,3 +10,50 @@ app.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
 
 app.listen(port);
 console.log('API funcionando!');
+
+const mysql = require('mysql2/promise');
+ 
+const client = mysql.createPool(process.env.CONNECTION_STRING);
+
+async function selectUsuarios(idEvento) {
+    var res;
+
+    if (!idEvento) {
+        res = await client.query('SELECT * FROM usuarios');
+    } else {
+        res = await client.query(`
+            SELECT usuarios.* FROM usuarios
+            INNER JOIN eventos_has_usuarios
+            ON usuarios.id_usuario = eventos_has_usuarios.usuarios_id_usuario
+            WHERE eventos_has_usuarios.eventos_id_evento = ?
+        `, [idEvento]);
+    }
+    return res[0];
+}
+
+async function selectEventos(idUsuario) {
+    var res;
+
+    if (!idUsuario) {
+        res = await client.query('SELECT * FROM eventos');
+    } else {
+        res = await client.query(`
+            SELECT eventos.* FROM eventos
+            INNER JOIN eventos_has_usuarios
+            ON eventos.id_evento = eventos_has_usuarios.eventos_id_evento
+            WHERE eventos_has_usuarios.usuarios_id_usuario = ?
+        `, [idUsuario]);
+    }
+    
+    return res[0];
+}
+
+app.get('/usuarios', async (req, res) => {
+    const usuarios = await selectUsuarios(req.query.id_evento);
+    res.json(usuarios);
+});
+
+app.get('/eventos', async (req, res) => {
+    const eventos = await selectEventos(req.query.id_usuario);
+    res.json(eventos);
+});
