7ec86842430efadfe7728a64fb5a7cad75e3087a
caio Fabio gregio <caio.f04@aluno.ifsc.edu.br>
2023-08-22 11:17:04 -0300

    insumo boy

---
+++ src/routes/insumo.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const router = express.Router()
+
+async function selecetInsumos(id_insumo) {
+    var res;
+    return res[0];
+}
+
+router.get('/', verifyToken, async (req, res) => {
+    
+});
+
+
+router.post('/', verifyToken, async (req, res) => {
+   
+    
+});
+
+
+
+
+
+
+
+
+
+export default router;
